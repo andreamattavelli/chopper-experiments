f24e1642de35a29f2cdd8599b524a09fa968c4cb
David Trabish <davidtr1037@gmail.com>
2017-07-29 18:54:43 +0300

    removing run scripts

--- libtasn1/CVE-2015-3622/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-time klee \
-    -exit-on-error-type=Ptr \
-    -libc=uclibc --posix-runtime \
-    -search=random-state \
-    -skip-functions=_asn1_set_value:872,_asn1_append_value,asn1_delete_structure \
-    -inline=memcpy,memset \
-    test.bc 30 1>out.log
