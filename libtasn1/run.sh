33781cc41a75041d773ef96c81663e743621475c
Andrea Mattavelli <andreamattavelli@gmail.com>
2017-10-07 11:24:55 +0100

    Fixed function names

--- libtasn1/run.sh
+++ libtasn1/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-run-experiment()
+run_experiment()
 {
 echo "* Running experiment $1"
 cd $1
@@ -8,27 +8,27 @@ make
 make all-klee
 
 make all-cse
-terminate-cse cse-no-searcher
+terminate_cse cse-no-searcher
 
 make KSLICE="-split-search -split-ratio=10" all-cse
-terminate-cse cse-split-searcher-10
+terminate_cse cse-split-searcher-10
 
 make KSLICE="-split-search -split-ratio=20" all-cse
-terminate-cse cse-split-searcher-20
+terminate_cse cse-split-searcher-20
 
 make KSLICE="-split-search -split-ratio=30" all-cse
-terminate-cse cse-split-searcher-30
+terminate_cse cse-split-searcher-30
 
 make KSLICE="-split-search -split-ratio=40" all-cse
-terminate-cse cse-split-searcher-40
+terminate_cse cse-split-searcher-40
 
 make KSLICE="-split-search -split-ratio=50" all-cse
-terminate-cse cse-split-searcher-50
+terminate_cse cse-split-searcher-50
 
 cd ..
 }
 
-terminate-cse()
+terminate_cse()
 {
 mkdir $1
 mv cse-run-* $1
@@ -37,7 +37,7 @@ mv run-cse-* $1
 
 
 ## MAIN
-run-experiment CVE-2012-1569
-run-experiment CVE-2014-3467
-run-experiment CVE-2015-2806
-run-experiment CVE-2015-3622
+run_experiment CVE-2012-1569
+run_experiment CVE-2014-3467
+run_experiment CVE-2015-2806
+run_experiment CVE-2015-3622
