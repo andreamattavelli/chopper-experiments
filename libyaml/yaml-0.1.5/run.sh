8895d6e5581f38535d23d446b74bba261bc57838
David Trabish <davidtr1037@gmail.com>
2018-01-07 17:08:44 +0200

    adding file

---
+++ libyaml/yaml-0.1.5/run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+time klee \
+    -max-time=3600 \
+    -max-memory=4096 \
+    -exit-on-error-type=Assert \
+    -libc=uclibc \
+    -simplify-sym-indices \
+    -search=nurs:covnew \
+    -recovery-search=dfs \
+    -skip-functions=yaml_parser_update_buffer:864,yaml_parser_fetch_stream_start:870 \
+    final.bc 10
+
+#time klee \
+#    -max-time=3600 \
+#    -libc=uclibc \
+#    -simplify-sym-indices \
+#    -search=nurs:covnew \
+#    final.bc 40
