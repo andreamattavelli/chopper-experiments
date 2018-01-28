3497b3e93adafa007d6e9e6910deb194614b31a5
user <davidtr1037@gmail.com>
2018-01-28 21:29:00 +0200

    updating run script

--- libyaml/yaml-0.1.5/run.sh
+++ libyaml/yaml-0.1.5/run.sh
@@ -3,17 +3,16 @@
 time klee \
     -max-time=3600 \
     -max-memory=4096 \
-    -exit-on-error-type=Assert \
     -libc=uclibc \
     -simplify-sym-indices \
     -search=nurs:covnew \
     -recovery-search=dfs \
     -skip-functions=yaml_parser_update_buffer:864,yaml_parser_fetch_stream_start:870 \
-    final.bc 40
+    final.bc 64
 
 time klee \
     -max-time=3600 \
     -libc=uclibc \
     -simplify-sym-indices \
     -search=nurs:covnew \
-    final.bc 40
+    final.bc 64
