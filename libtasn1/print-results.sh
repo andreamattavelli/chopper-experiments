c10472509eaf66fc0cce80e38acc7e9dc16c6110
Jordy Ruiz <jruiz@ic.ac.uk>
2020-01-14 19:01:15 +0000

    Tweaked the print_results.sh script to work with CVE-2014-3467
    - Now outputs three .csv files for this benchmark, one for each bug
    - Some pretty printing on stderr

--- libtasn1/print-results.sh
+++ libtasn1/print-results.sh
@@ -6,9 +6,9 @@ print_single_result()
 {
     paste -d ',' \
     <( echo "$2" ) \
-    <( grep Elapsed $1*/*-coverage/info | cut -d ' ' -f2 ) \
-    <( grep Elapsed $1*/*-dfs/info | cut -d ' ' -f2 ) \
-    <( grep Elapsed $1*/*-random/info | cut -d ' ' -f2 )
+    <( grep Elapsed $1*/*-coverage$3/info | cut -d ' ' -f2 ) \
+    <( grep Elapsed $1*/*-dfs$3/info | cut -d ' ' -f2 ) \
+    <( grep Elapsed $1*/*-random$3/info | cut -d ' ' -f2 )
 }
 
 # Parameters: 1- directory, 2- label
@@ -17,9 +17,9 @@ print_multiple_result()
 {
     paste -d ',' \
 	  <( for num in `seq 10 10 50`; do echo "$2 $num"; done ) \
-	  <( grep Elapsed $1-*/*-coverage/info | cut -d ' ' -f2 ) \
-	  <( grep Elapsed $1-*/*-dfs/info | cut -d ' ' -f2 ) \
-	  <( grep Elapsed $1-*/*-random/info | cut -d ' ' -f2 )
+	  <( grep Elapsed $1-*/*-coverage$3/info | cut -d ' ' -f2 ) \
+	  <( grep Elapsed $1-*/*-dfs$3/info | cut -d ' ' -f2 ) \
+	  <( grep Elapsed $1-*/*-random$3/info | cut -d ' ' -f2 )
 }
 
 print_header()
@@ -29,19 +29,28 @@ print_header()
 
 print_results()
 {
+    if [ -n "$2" ]
+    then
+        SUBBENCH="-$2"
+    else
+        SUBBENCH=""
+    fi
+    echo "===== Running on '$1$SUBBENCH' =====" >&2
     cd $1
     print_header
-    print_single_result   "klee" "KLEE"
-    print_single_result   "cse-no-searcher" "STANDARD"
-    print_multiple_result "cse-split-searcher" "SPLIT-SEARCH"
-    print_multiple_result "cse-recovery-searcher-rp" "RECOVERY-SEARCH-RP"
-    print_multiple_result "cse-recovery-searcher-dfs" "RECOVERY-SEARCH-DFS"
+    print_single_result   "klee" "KLEE" $SUBBENCH
+    print_single_result   "cse-no-searcher" "STANDARD" $SUBBENCH
+    print_multiple_result "cse-split-searcher" "SPLIT-SEARCH" $SUBBENCH
+    print_multiple_result "cse-recovery-searcher-rp" "RECOVERY-SEARCH-RP" $SUBBENCH
+    print_multiple_result "cse-recovery-searcher-dfs" "RECOVERY-SEARCH-DFS" $SUBBENCH
     cd ..
 }
 
 
 ## MAIN
 print_results CVE-2012-1569 > CVE-2012-1569.csv
-print_results CVE-2014-3467 > CVE-2014-3467.csv
+print_results CVE-2014-3467 1 > CVE-2014-3467_1.csv
+print_results CVE-2014-3467 2 > CVE-2014-3467_2.csv
+print_results CVE-2014-3467 3 > CVE-2014-3467_3.csv
 print_results CVE-2015-2806 > CVE-2015-2806.csv
 print_results CVE-2015-3622 > CVE-2015-3622.csv
